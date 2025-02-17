69225f538e7a6196908d91c2c187b46dad122d75
mukiralad <67241568+mukiralad@users.noreply.github.com>
2025-02-16 22:55:11 -0500

    Added routes for setting, getting, and deleting cookies

--- index.js
+++ index.js
@@ -8,6 +8,24 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Welcome to the Cookie Manager!');
 });
+// Set a cookie
+app.post('/set-cookie', (req, res) => {
+    const { name, value } = req.body;
+    res.cookie(name, value, { httpOnly: true });
+    res.send(`Cookie ${name} set!`);
+});
+
+// Get all cookies
+app.get('/get-cookies', (req, res) => {
+    res.json(req.cookies);
+});
+
+// Delete a cookie
+app.delete('/delete-cookie/:name', (req, res) => {
+    const { name } = req.params;
+    res.clearCookie(name);
+    res.send(`Cookie ${name} deleted!`);
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
