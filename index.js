652bc9a45d693ff4aa17a67a1849e0d8df3bc772
mukiralad <67241568+mukiralad@users.noreply.github.com>
2025-02-16 22:57:28 -0500

    Added frontend for cookie management

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.delete('/delete-cookie/:name', (req, res) => {
     res.clearCookie(name);
     res.send(`Cookie ${name} deleted!`);
 });
+app.use(express.static('public'));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
